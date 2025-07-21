e0bb5593dfeb58dc2087dfa90983f51e3531025f
mariasefitha <mariasefitha@gmail.com>
2025-07-21 11:40:42 +0700

    railway deploy 2

--- router/router.go
+++ router/router.go
@@ -7,25 +7,21 @@ import (
 )
 
 func RegisterRoutes(r *gin.RouterGroup) {
-	api := r.Group("/api")
-	{
-		// CATEGORY ROUTES
-		api.POST("/categories", controller.CreateCategory)
-		// api.GET("/categories", controller.GetCategories)
-		// api.GET("/categories/:id", controller.GetCategoryByID)
-		api.GET("/categories", controller.GetAllCategories)
-		//api.DELETE("/categories/:id", controller.DeleteCategory)
+	// CATEGORY ROUTES
+	r.POST("/categories", controller.CreateCategory)
+	// api.GET("/categories", controller.GetCategories)
+	// api.GET("/categories/:id", controller.GetCategoryByID)
+	r.GET("/categories", controller.GetAllCategories)
+	//api.DELETE("/categories/:id", controller.DeleteCategory)
 
-		//KATEGORI BUKU
-		api.POST("/books", controller.CreateBook)
-		api.GET("/books", controller.GetAllBooks)
-		api.GET("/books/:id", controller.GetBookByID)
-		api.PUT("/books", controller.UpdateBook)
-		api.DELETE("/books", controller.DeleteBook)
+	//KATEGORI BUKU
+	r.POST("/books", controller.CreateBook)
+	r.GET("/books", controller.GetAllBooks)
+	r.GET("/books/:id", controller.GetBookByID)
+	r.PUT("/books", controller.UpdateBook)
+	r.DELETE("/books", controller.DeleteBook)
 
-		//KATEGORI + BUKU
-		api.GET("/categories/:id/books", controller.GetBooksByCategory)
-
-	}
+	//KATEGORI + BUKU
+	r.GET("/categories/:id/books", controller.GetBooksByCategory)
 
 }
